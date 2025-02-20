6c99f10d010b9cf96b4f3250cc083b71dfeac78c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2025-02-20 11:24:54 +0600

    merchant account create order

--- src/MerchantAccount.php
+++ src/MerchantAccount.php
@@ -3,6 +3,8 @@
 namespace Anik\Paperfly\Laravel;
 
 use Anik\Paperfly\Client;
+use Anik\Paperfly\Response;
+use Anik\Paperfly\Transfers\CreateOrder;
 
 class MerchantAccount
 {
@@ -11,5 +13,22 @@ class MerchantAccount
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    /**
+     * @throws \Anik\Paperfly\Laravel\PaperflyException
+     */
+    public function createOrder($order): Response
+    {
+        if ($order instanceof CreateOrder) {
+            return $this->client->gracefulTransfer($order);
+        } elseif (is_array($order)) {
+            return $this->client->gracefulTransfer(CreateOrder::buildFrom($order));
+        }
+
+        throw new PaperflyException(sprintf(
+            'Create order accepts either an array or a "%s" object',
+            CreateOrder::class
+        ));
     }
 }
